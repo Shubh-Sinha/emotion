6bb03e0c72fb6c78a83ac737275d2459e986809a
Shubh-Sinha <80043682+Shubh-Sinha@users.noreply.github.com>
2021-03-09 21:16:46 +0530

    Add files via upload

---
+++ emotion.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import numpy as np
+from keras.models import Sequential
+import keras.utils
+from keras.layers.core import Flatten, Dense, Dropout
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
+from keras.optimizers import SGD
+import cv2
+
+emotion_data = pd.read_csv('fer2013.csv')
+print('emotion_data')
+
+X_train = []
+y_train = []
+X_test = []
+y_test = []
+for index, row in emotion_data.iterrows():
+    k = row['pixels'].split(" ")
+    if row['Usage'] == 'Training':
+        X_train.append(np.array(k))
+        y_train.append(row['emotion'])
+    elif row['Usage'] == 'PublicTest':
+        X_test.append(np.array(k))
+        y_test.append(row['emotion'])
+
+X_train = np.array(X_train)
+y_train = np.array(y_train)
+X_test = np.array(X_test)
+y_test = np.array(y_test)
+
+X_train = X_train.reshape(X_train.shape[0], 48, 48, 1)
+X_test = X_test.reshape(X_test.shape[0], 48, 48, 1)
+
+y_train = keras.utils.to_categorical(y_train, num_classes=7)
+y_test = keras.utils.to_categorical(y_test, num_classes=7)
+
+model = Sequential()
+model.add(ZeroPadding2D((1,1),input_shape=(48,48,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(64, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(128, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(256, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(ZeroPadding2D((1,1)))
+model.add(Convolution2D(512, 3, 3, activation='relu'))
+model.add(MaxPooling2D((2,2), strides=(2,2)))
+
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(7, activation='softmax'))
+
+model.compile(optimizer='sgd',loss='categorical_crossentropy',metrics=['accuracy'])
+
+loss_and_metrics = model.evaluate(X_test,y_test)
+print(loss_and_metrics)
+
+model_json = model.to_json()
+with open("model.json", "w") as json_file:
+json_file.write(model_json)
+model.save_weights("model.h5")
+print("Saved model to disk")
